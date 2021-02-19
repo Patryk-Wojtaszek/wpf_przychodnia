4357cbe98712a16014f40c125bc3d51eac815044
sniqq1 <pat.wojtaszek@gmail.com>
2021-02-19 23:28:45 +0100

    good_night

--- wpf_przychodnia_1/ListaPacjentow.xaml.cs
+++ wpf_przychodnia_1/ListaPacjentow.xaml.cs
@@ -34,5 +34,66 @@ namespace wpf_przychodnia_1
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
         }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            PrzychodniaEntities przychodniaEntities = new PrzychodniaEntities();
+            this.Grid_Pacjenci.ItemsSource = przychodniaEntities.Pacjenci.ToList();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+      
+            
+            PrzychodniaEntities przychodniaEntities = new PrzychodniaEntities();
+            Pacjenci nowyPacjent = new Pacjenci()
+            {
+                ID_pacjenta = (przychodniaEntities.Pacjenci.Count() + 1),
+                Imie = txt_imie.Text,
+                Nazwisko = txt_nazwisko.Text,
+                Pesel = txt_pesel.Text
+                
+                
+                
+              
+               
+                
+
+            };
+
+            przychodniaEntities.Pacjenci.Add(nowyPacjent);
+            przychodniaEntities.SaveChanges();
+            
+   
+
+        }
+
+        private void Grid_Pacjenci_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.Grid_Pacjenci.SelectedIndex >= 0)
+            {
+                if (this.Grid_Pacjenci.SelectedItems.Count >= 0)
+                {
+                    if (this.Grid_Pacjenci.SelectedItems[0].GetType() == typeof(Pacjenci))
+                    {
+                        Pacjenci p = (Pacjenci)this.Grid_Pacjenci.SelectedItems[0];
+                        this.txt_imie_2.Text = p.Imie;
+                        this.txt_nazwisko_2.Text = p.Nazwisko;
+                        this.txt_pesel_2.Text = p.Pesel;
+                    }
+                }
+            }
+        }
+
+        private void Modify_Click(object sender, RoutedEventArgs e)
+        {
+    
+        }
+
+        private void txt_imie_Copy_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
